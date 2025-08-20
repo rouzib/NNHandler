e640f4a0e50b5de1e74570134f1afbb742de9753
Rouzib <npayot@gmail.com>
2025-08-20 18:53:04 -0400

    Update

--- tests/ddp.sh
+++ tests/ddp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=0-00:05:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-gpu=1
+#SBATCH --ntasks=4
 #SBATCH --gpus-per-node=4
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4G
