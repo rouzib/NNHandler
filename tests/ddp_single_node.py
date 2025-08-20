37ba377cadc9d2719963ec3505f0eb1f3a8c0adc
Rouzib <npayot@gmail.com>
2025-08-20 18:14:31 -0400

    Update

--- tests/ddp_single_node.py
+++ tests/ddp_single_node.py
@@ -7,13 +7,12 @@ sys.path.insert(0, str(project_root))
 
 import torch
 
-from src.nn_handler.utils.ddp import _initialize_distributed
-
-
+from src.nn_handler.utils.ddp import _initialize_distributed, _is_env_distributed
 
 print(os.environ["CUDA_VISIBLE_DEVICES"])
 
 print(f"{torch.cuda.device_count()} GPUs available. {torch.cuda.is_available()=}")
+print(f"{_is_env_distributed()=}")
 _initialize_distributed()
 
 print([k for k in os.environ.keys() if "SLURM" in k])
