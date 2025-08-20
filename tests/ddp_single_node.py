83d40082943736efe6fbe68ab65434fd0d99b6ee
Rouzib <npayot@gmail.com>
2025-08-20 18:05:41 -0400

    Update

--- tests/ddp_single_node.py
+++ tests/ddp_single_node.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import sys
+import os
 
 project_root = Path(__file__).resolve().parent.parent
 sys.path.insert(0, str(project_root))
@@ -8,4 +9,7 @@ import torch
 
 from src.nn_handler import NNHandler, initialize_ddp
 
+print(os.environ["CUDA_VISIBLE_DEVICES"])
+
+print(f"{torch.cuda.device_count()} GPUs available. {torch.cuda.is_available()=}")
 initialize_ddp()
