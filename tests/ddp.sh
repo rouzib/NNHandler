8eb4a81a299050cd20f18f403ba1d504ad6ccc81
Rouzib <npayot@gmail.com>
2025-08-20 18:26:33 -0400

    update

---
+++ tests/ddp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --time=0-00:05:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=4G
+#SBATCH --account=aip-lplevass
+#SBATCH --chdir=/home/r/rouzib/links/scratch
+#SBATCH --output=/home/r/rouzib/links/scratch/ddp_%j.out
+#SBATCH --mail-user=npayot@gmail.com
+#SBATCH --mail-type=FAIL,END,BEGIN
+
+module load gcc cuda/12.2 nccl/2.18.3 python/3.11
+
+source /home/r/rouzib/links/scratch/nn_handler/bin/activate
+
+srun /home/r/rouzib/links/scratch/NNHandler/tests/ddp_single_node.py
