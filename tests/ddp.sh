cf1db02593c3a78cd68b1cb949c770bae55b3bfd
Rouzib <npayot@gmail.com>
2025-08-20 18:27:40 -0400

    update

--- tests/ddp.sh
+++ tests/ddp.sh
@@ -2,8 +2,8 @@
 #SBATCH --time=0-00:05:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=4
-#SBATCH --gpus-per-task=1
-#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-gpu=1
 #SBATCH --mem-per-cpu=4G
 #SBATCH --account=aip-lplevass
 #SBATCH --chdir=/home/r/rouzib/links/scratch
