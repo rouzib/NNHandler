7ed8119b4421634c7825d7ba286d03eab6a25d80
Rouzib <npayot@gmail.com>
2025-08-20 20:53:20 -0400

    Update

--- src/nn_handler/utils/ddp.py
+++ src/nn_handler/utils/ddp.py
@@ -83,8 +83,8 @@ def _is_env_distributed() -> bool:
             world_size = int(os.environ['SLURM_NTASKS'])
             num_gpus_on_node = torch.cuda.device_count()
             print(f"{num_gpus_on_node = }")
-            if num_gpus_on_node > 0:
-                world_size *= num_gpus_on_node
+            # if num_gpus_on_node > 0:
+            #     world_size *= num_gpus_on_node
         except ValueError:
             print("WARN: Slurm env vars could not be parsed as integers.")
             return False
