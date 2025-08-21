fec863481c101030f1ebd9e9a950c3fcc7dad304
Rouzib <npayot@gmail.com>
2025-08-21 12:00:06 -0400

    Update

--- tests/ddp.sh
+++ tests/ddp.sh
@@ -12,10 +12,9 @@
 
 module load gcc cuda/12.2 nccl/2.18.3 python/3.11
 
-export TORCH_NCCL_BLOCKING_WAIT=1
-export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
-export NCCL_DEBUG=INFO
-
 source /home/r/rouzib/links/scratch/nn_handler/bin/activate
+
+echo $SLURM_JOB_NUM_NODES
+echo $SLURM_GPUS_PER_NODE
 
 srun torchrun --nnodes=$SLURM_JOB_NUM_NODES --nproc_per_node=$SLURM_GPUS_PER_NODE /home/r/rouzib/links/scratch/NNHandler/tests/ddp_single_node.py
