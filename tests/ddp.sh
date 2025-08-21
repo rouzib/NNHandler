85476bf6f8d3f3ffbbc825ac525bc90169528496
Rouzib <npayot@gmail.com>
2025-08-20 21:34:02 -0400

    Update

--- tests/ddp.sh
+++ tests/ddp.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=0-00:05:00
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=4
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-gpu=1
 #SBATCH --mem-per-cpu=4G
 #SBATCH --account=aip-lplevass
 #SBATCH --chdir=/home/r/rouzib/links/scratch
@@ -14,4 +14,4 @@ module load gcc cuda/12.2 nccl/2.18.3 python/3.11
 
 source /home/r/rouzib/links/scratch/nn_handler/bin/activate
 
-srun torchrun --nnodes=1 --nproc_per_node=4 /home/r/rouzib/links/scratch/NNHandler/tests/ddp_single_node.py
+srun torchrun --nnodes=$SLURM_JOB_NUM_NODES --nproc_per_node=$SLURM_GPUS_PER_NODE /home/r/rouzib/links/scratch/NNHandler/tests/ddp_single_node.py
